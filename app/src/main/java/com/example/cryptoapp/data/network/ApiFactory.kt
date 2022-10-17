e827a163984b84537463cdaf8ba8fe5793df51a6
Dimadyuk <dimadyuk88@gmail.com>
2022-10-17 12:15:05 +0200

    lesson 159 change mappers and models

--- app/src/main/java/com/example/cryptoapp/data/network/ApiFactory.kt
+++ app/src/main/java/com/example/cryptoapp/data/network/ApiFactory.kt
@@ -1,13 +1,11 @@
 package com.example.cryptoapp.data.network
 
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiFactory {
 
     private const val BASE_URL = "https://min-api.cryptocompare.com/data/"
-    const val BASE_IMAGE_URL = "https://cryptocompare.com"
 
     private val retrofit = Retrofit.Builder()
         .addConverterFactory(GsonConverterFactory.create())
--- app/src/main/java/com/example/cryptoapp/domain/CoinInfo.kt
+++ app/src/main/java/com/example/cryptoapp/domain/CoinInfo.kt
@@ -4,10 +4,10 @@ data class CoinInfo(
     val fromSymbol: String,
     val toSymbol: String?,
     val price: String?,
-    val lastUpdate: Long?,
+    val lastUpdate: String,
     val highDay: String?,
     val lowDay: String?,
     val lastMarket: String?,
-    val imageURL: String?
+    val imageURL: String
 ) {
 }
